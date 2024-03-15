64faca600d83120a4401d7db025137d195e356f3
learnwithancor <ancorpoint0@gmail.com>
2024-03-16 05:26:10 +0600

    commited

--- src/AllRoutes/Routes.jsx
+++ src/AllRoutes/Routes.jsx
@@ -1,35 +1,33 @@
 import { createBrowserRouter } from "react-router-dom";
-import Layout from "./layout";
 import HomePage from "../Pages/HomePage";
 import ErrorPage from "../components/shared/ErrorPage";
+import Layout from "./layout";
 
 export const router = createBrowserRouter([
-    {
-      path: "/",
-      element: <Layout />,
-      children : [
-        {
-                path: "/",
-                element: <HomePage />,
-        },
-        {
-                path: "/events",
-                element: <div>Hello events page!</div>,
-        },
-        {
-                path: "/about",
-                element: <div>Hello about page!</div>,
-        },
-        {
-                path: "/contact",
-                element: <div>Hello contact page!</div>,
-        },
-      ]
-    },
-    {
-        path: "*",
-        element: <ErrorPage />,
-
-    }
-    
-  ]);
+  {
+    path: "/",
+    element: <Layout />,
+    children: [
+      {
+        path: "/",
+        element: <HomePage />,
+      },
+      {
+        path: "/events",
+        element: <div>Hello events page!</div>,
+      },
+      {
+        path: "/about",
+        element: <div>Hello about page!</div>,
+      },
+      {
+        path: "/contact",
+        element: <div>Hello contact page!</div>,
+      },
+    ],
+  },
+  {
+    path: "*",
+    element: <ErrorPage />,
+  },
+]);
--- src/components/shared/Header.jsx
+++ src/components/shared/Header.jsx
@@ -1,4 +1,3 @@
-import React from "react";
 import { NavLink } from "react-router-dom";
 
 import logo from '../../assets/logo.png'
@@ -13,21 +12,21 @@ const Header = () => {
         </div>
       <nav>
         <ul className="flex gap-x-10 text-[15px]">
-          <li className="hover:text-orange-700 duration-300">
+          <li className="hover:text-[#f45183] duration-300">
             <NavLink to="/">
               Home
             </NavLink>
           </li>
-          <li className="hover:text-orange-700 duration-300">
+          <li className="hover:text-[#f45183] duration-300">
             <NavLink to="/events">Events</NavLink>
           </li>
-          <li className="hover:text-orange-700 duration-300">
+          <li className="hover:text-[#f45183] duration-300">
             <NavLink to="/notice">Notice</NavLink>
           </li>
-          <li className="hover:text-orange-700 duration-300">
+          <li className="hover:text-[#f45183] duration-300">
             <NavLink to="/about">About</NavLink>
           </li>
-          <li className="hover:text-orange-700 duration-300">
+          <li className="hover:text-[#f45183] duration-300">
             <NavLink to="/contact">Contact</NavLink>
           </li>
         </ul>
