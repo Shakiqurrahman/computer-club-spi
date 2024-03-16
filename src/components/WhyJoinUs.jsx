23c42ee2933a3a09f4b101d3e0b9321d94979268
learnwithancor <ancorpoint0@gmail.com>
2024-03-17 01:27:20 +0600

    border contrast reduced

--- src/components/WhyJoinUs.jsx
+++ src/components/WhyJoinUs.jsx
@@ -8,15 +8,15 @@ const WhyJoinUs = () => {
         <div>
             <h2 className="text-center text-secondary text-4xl font-semibold mb-10">Why Join SPI Computer Club?</h2>
             <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <div className="group px-6 py-8 border border-gray-400 rounded-xl flex  flex-col items-center justify-center text-center hover:border-[#00066e]  duration-300">
+                <div className="group px-6 py-8 border border-gray-300 rounded-xl flex  flex-col items-center justify-center text-center hover:border-[#00066e]  duration-300">
                     <SiSkillshare className="mb-6 text-[80px] text-secondary"/>
                     <p className="group-hover:text-secondary duration-300 text-[14.5px] leading-relaxed">To build skills in various sectors including Content Writing, Photojournalism, Video Editing, Event Organizing, Volunteering, Development, UI/UX Design, and Graphic Design.</p>
                 </div>
-                <div className="group px-6 py-8 border border-gray-400 rounded-xl flex  flex-col items-center justify-center text-center hover:border-secondary  duration-300">
+                <div className="group px-6 py-8 border border-gray-300 rounded-xl flex  flex-col items-center justify-center text-center hover:border-secondary  duration-300">
                     <HiUserGroup className="mb-6 text-[80px] text-secondary"/>
                     <p className="group-hover:text-secondary duration-300 text-[14.5px] leading-relaxed">To connect with a community of senior, junior and respected teachers. Build harmony and get ideas about the demanding skills of the current and future professional domain.</p>
                 </div>
-                <div className="group px-6 py-8 border border-gray-400 rounded-xl flex  flex-col items-center justify-center text-center hover:border-[#00066e]  duration-300">
+                <div className="group px-6 py-8 border border-gray-300 rounded-xl flex  flex-col items-center justify-center text-center hover:border-[#00066e]  duration-300">
                     <MdEmojiEvents className="mb-6 text-[80px] text-secondary"/>
                     <p className="group-hover:text-secondary duration-300 text-[14.5px] leading-relaxed">To organize events national events such as Computer Science Fest, Computer Gaming Fest and intra ACC events like Jarvis - An Intra ACC Carnival and participate in regularly organized workshops and seminars for ACC members.</p>
                 </div>
