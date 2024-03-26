ed1331d7ba9dedb9ef4cac8e07eeb1b1ffffa21f
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-03-27 03:22:26 +0600

    contact updated

--- src/Pages/ContactPage.jsx
+++ src/Pages/ContactPage.jsx
@@ -1,9 +1,8 @@
-import Lottie from "lottie-react";
 import React from "react";
 import { FaHome } from "react-icons/fa";
 import { MdNavigateNext } from "react-icons/md";
 import { Link } from "react-router-dom";
-import contactGIF from "../assets/GIF/email2.json";
+import ContactForm from "../components/ContactForm";
 
 const ContactPage = () => {
   return (
@@ -22,59 +21,8 @@ const ContactPage = () => {
         <MdNavigateNext size={22} />
         <span className="text-[#2030e0]">Contact</span>
       </h3>
-      <div className="md:my-32 mt-10 mb-32 flex flex-col lg:flex-row items-center justify-center sm:gap-20 gap-10">
-        <div className="w-4/5 flex justify-center">
-          <Lottie animationData={contactGIF} />
-        </div>
-        <form className="w-full flex flex-col gap-5  bg-[#e8e4fb]/85 rounded-[30px] sm:p-16 p-8">
-          <div className="mb-5">
-            {/* <p className="text-center uppercase tracking-widest mb-1 text-secondary">
-              Any Query?
-            </p> */}
-            <h3 className="text-center text-3xl font-bold text-[#0067f6]">
-              Contact Us
-            </h3>
-          </div>
-          <div className="flex flex-col sm:flex-row gap-5">
-            <input
-              className="px-3 py-3 w-full outline-none  rounded-md"
-              type="text"
-              name="Name"
-              placeholder="Your Name"
-              required
-            />
-            <input
-              className="px-3 py-3 w-full outline-none rounded-md"
-              type="email"
-              name="Email"
-              placeholder="Your Email"
-              required
-            />
-          </div>
-
-          <input
-            className="px-3 py-3 w-full outline-none rounded-md"
-            type="text"
-            name="Subject"
-            placeholder="Email Subject"
-            required
-          />
-
-          <textarea
-            className="px-3 py-3 outline-none rounded-md resize-none h-28 sm:h-auto"
-            name="Message"
-            cols="30"
-            rows="6"
-            placeholder="Message"
-            required
-          ></textarea>
-          <button
-            type="submit"
-            className="mt-3 py-3 px-4 w-48 mx-auto border border-transparent text-white bg-blue-600 hover:bg-white hover:text-[#0067f6] hover:border-[#0067f6] text-lg font-semibold  rounded-md duration-300 "
-          >
-            Send Message
-          </button>
-        </form>
+      <div className="md:my-32 mt-10 mb-32">
+      <ContactForm />
       </div>
     </>
   );
---
+++ src/components/ContactForm.jsx
@@ -0,0 +1,64 @@
+import React from "react";
+import Lottie from "lottie-react";
+import contactGIF from "../assets/GIF/contact.json";
+
+const ContactForm = () => {
+  return (
+    <div className="flex flex-col lg:flex-row items-center justify-center sm:gap-20 gap-10">
+      <div className="w-full lg:w-[90%] flex justify-center">
+        <Lottie animationData={contactGIF} />
+      </div>
+      <form className="w-full flex flex-col gap-5  bg-[#e8e4fb]/85 rounded-[30px] sm:p-16 p-8">
+        <div className="mb-5">
+          {/* <p className="text-center uppercase tracking-widest mb-1 text-secondary">
+              Any Query?
+            </p> */}
+          <h3 className="text-center text-3xl font-bold text-blue-600">
+            Contact Us
+          </h3>
+        </div>
+        <div className="flex flex-col sm:flex-row gap-5">
+          <input
+            className="px-3 py-3 w-full outline-none  rounded-md"
+            type="text"
+            name="Name"
+            placeholder="Your Name"
+            required
+          />
+          <input
+            className="px-3 py-3 w-full outline-none rounded-md"
+            type="email"
+            name="Email"
+            placeholder="Your Email"
+            required
+          />
+        </div>
+
+        <input
+          className="px-3 py-3 w-full outline-none rounded-md"
+          type="text"
+          name="Subject"
+          placeholder="Email Subject"
+          required
+        />
+
+        <textarea
+          className="px-3 py-3 outline-none rounded-md resize-none h-28 sm:h-auto"
+          name="Message"
+          cols="30"
+          rows="6"
+          placeholder="Message"
+          required
+        ></textarea>
+        <button
+          type="submit"
+          className="mt-3 py-3 px-4 w-48 mx-auto border border-transparent text-white bg-blue-600 hover:bg-white hover:text-blue-600 hover:border-blue-600 text-lg font-semibold  rounded-md duration-300 "
+        >
+          Send Message
+        </button>
+      </form>
+    </div>
+  );
+};
+
+export default ContactForm;
