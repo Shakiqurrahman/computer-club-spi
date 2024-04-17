2ed8882f2b45a461493c22a06aee1d691f52eb91
shakiqurrahman <rahmanshakiqur@gmail.com>
2024-04-17 15:52:58 +0600

    breadcrumb remove for sm

--- src/Pages/ResultPage.jsx
+++ src/Pages/ResultPage.jsx
@@ -7,20 +7,22 @@ import ResultHero from "../components/ResultHero";
 const ResultPage = () => {
   return (
     <>
-      <h1 className="text-center font-semibold text-3xl mb-1 text-secondary">
-        Results
-      </h1>
-      <h3 className="text-md font-normal flex justify-center items-center">
-        <Link
-          className="hover:text-[#2030e0] duration-300 flex items-center gap-1.5"
-          to="/"
-        >
-          <FaHome size={18} />
-          Home
-        </Link>
-        <MdNavigateNext size={22} />
-        <span className="text-[#2030e0]">Results</span>
-      </h3>
+      <div className="hidden sm:block">
+        <h1 className="text-center font-semibold text-3xl mb-1 text-secondary">
+          Results
+        </h1>
+        <h3 className="text-md font-normal flex justify-center items-center">
+          <Link
+            className="hover:text-[#2030e0] duration-300 flex items-center gap-1.5"
+            to="/"
+          >
+            <FaHome size={18} />
+            Home
+          </Link>
+          <MdNavigateNext size={22} />
+          <span className="text-[#2030e0]">Results</span>
+        </h3>
+      </div>
       <div className="mt-8 mb-32">
         <ResultHero />
       </div>
